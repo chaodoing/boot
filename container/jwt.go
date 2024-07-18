9865a037916c72859ba308399dccb978afd603fd
砉戟 <chaodoing@hotmail.com>
2024-07-18 09:11:40 +0800

    修改jwt方法

--- container/jwt.go
+++ container/jwt.go
@@ -10,10 +10,10 @@ import (
 
 // Jwt 结构体用于管理 JWT（JSON Web Token）的中间件和相关配置。
 type Jwt struct {
-	jet        *jwt.Middleware // JWT 中间件实例
-	secret     []byte          // 用于签名JWT的密钥
-	duration   time.Duration   // JWT的过期时间
-	Middleware iris.Handler    // JWT中间件的处理函数
+	jet      *jwt.Middleware // JWT 中间件实例
+	secret   []byte          // 用于签名JWT的密钥
+	duration time.Duration   // JWT的过期时间
+	Handle   iris.Handler    // JWT中间件的处理函数
 }
 
 // NewJwt 创建并返回一个新的 Jwt 实例，配置了指定的密钥和过期时间。
@@ -31,12 +31,32 @@ func NewJwt(secret string, duration time.Duration) *Jwt {
 			o.O(ctx, 401, err.Error())
 		},
 	})
+	
 	return &Jwt{
-		secret:     []byte(secret),
-		duration:   duration,
-		jet:        jet,
-		Middleware: jet.Serve,
+		secret:   []byte(secret),
+		duration: duration,
+		jet:      jet,
+		Handle:   jet.Serve,
 	}
+}
+
+// ErrHandle 设置JWT模块的错误处理函数。
+// 该函数接收一个处理 Iris 上下文和错误的函数作为参数，并返回Jwt实例的指针。
+// 通过这个方法，可以自定义JWT验证过程中的错误处理逻辑。
+func (j *Jwt) ErrHandle(handle func(iris.Context, error)) *Jwt {
+	j.jet.Config.ErrorHandler = handle
+	return j
+}
+
+// Middleware 返回一个JWT中间件，该中间件可用于验证和处理JWT令牌。
+//
+// 该方法使得Jwt结构体实例能够作为中间件提供给HTTP处理链，以便在请求处理过程中进行JWT令牌的验证和解析。
+// 返回的jwt.Middleware实例封装了JWT验证的具体逻辑，包括令牌的解析、验证和错误处理。
+//
+// 返回:
+//   *jwt.Middleware - JWT中间件实例，用于HTTP请求处理。
+func (j *Jwt) Middleware() *jwt.Middleware {
+	return j.jet
 }
 
 // Get 从上下文中提取JWT，并返回其包含的"dial"字段的值。
